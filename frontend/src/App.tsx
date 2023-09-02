0050fad0491187f1e269846049277164474f7cfa
Mohammed Farhan Islam <farhanislam670@gmail.com>
2023-09-02 10:13:04 +0600

    Configured frontend structure.

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+
+function App() {
+  return (
+    <>
+      <h1 className="font-semibold m-5">I see</h1>
+      <Button>Button</Button>
+    </>
+  );
+}
+
+export default App;
